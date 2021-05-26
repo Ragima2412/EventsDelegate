6383bf3651631851ab10b41d76a30d64331761f4
Ragima <ragima2412@gmail.com>
2021-05-26 22:46:09 +0400

    DZ

---
+++ EventNew/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace EventNew 
+{ 
+    class Ping 
+{
+        public delegate void Hit(string message);
+        public event Hit Vivod;
+        public void Hits()
+        {
+            Vivod?.Invoke("Ping received Pong");
+        }
+    }
+class Pong
+{
+        public delegate void Hit(string message);
+        public event Hit Vivod;
+        public void Hits()
+        {
+            Vivod?.Invoke("Pong received Ping");
+        }
+    }
+    class Program
+    {
+        public static void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+        static void Main(string[] args)
+        {
+           string swt;
+            bool result1 = true;
+            Console.WriteLine("Welcome to Ping Pong Game");
+            do {
+                Console.WriteLine("Choose:\n 1.Start the game\n 2.Exit. ");
+                swt = Console.ReadLine();
+                switch (swt) 
+                {
+                    case "1":
+                        bool result = true; 
+                        do
+                        {
+                            Console.WriteLine("Seychas vash xod. Dla udara vvedite lubuyu knopku. Dla prekraweniya igri vvedite knopku E");
+                            string Hit;
+                            do
+                            {
+                                Hit = Console.ReadLine();                                
+                                if (Hit.Length > 1)
+                                {
+                                    Console.WriteLine("Enter correct data");
+                                }
+                            }
+                            while (Hit.Length > 1); 
+
+                            if (Hit != "E")
+                            {                               
+                                Ping ping = new Ping();
+                                Pong pong = new Pong();
+
+                                ping.Vivod += DisplayMessage;
+                                pong.Vivod += DisplayMessage;
+
+                                ping.Hits();
+                                Console.WriteLine("The second player made a move");
+                                pong.Hits();
+                            }
+                            else 
+                            {
+                                result = false;                            
+                            }
+                        }
+                        while (result);
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Exit from game");
+                        result1 = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Choose only 1 or 2" );
+                        break;
+                }
+            }
+            while (result1);
+        }
+    }
+}
---
+++ EvetsPingPong/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace EvetsPingPong
+{
+    class Ping
+    {
+        public delegate void Hit(string message);
+        public event Hit Vivod;
+        public void Hits()
+        {
+            Vivod?.Invoke("Ping received Pong");
+        }
+    }
+    class Pong
+    {
+        public delegate void Hit(string message);
+        public event Hit Vivod;
+        public void Hits()
+        {
+            Vivod?.Invoke("Pong received Ping");
+        }
+    }
+    class Program
+    {
+        static int GameMove()
+        {
+            int _GameMove = 0;
+            bool IsNumber = false;
+            while (!IsNumber)
+            {
+                Console.WriteLine("How many steps will be taken in the game ");
+                bool result = Int32.TryParse(Console.ReadLine(), out _GameMove);
+                if (result == false)
+                {
+                    Console.WriteLine("You enter inccorect data");
+                }
+                else
+                {
+                    IsNumber = true;
+                }
+            }
+
+            return _GameMove;
+        }
+        public static void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to PING PONG GAME\n");
+            int _GameMove = GameMove();
+
+            Ping ping1 = new Ping();
+            Pong pong1 = new Pong();
+            Ping ping2 = new Ping();
+            Pong pong2 = new Pong();
+
+            ping1.Vivod += DisplayMessage;
+            pong1.Vivod += DisplayMessage;
+
+            Console.WriteLine("First team\n *****");
+            for (int i = 0; i < _GameMove; i++)
+            {
+                ping1.Hits();
+                pong1.Hits();
+            }
+            ping2.Vivod += DisplayMessage;
+            pong2.Vivod += DisplayMessage;
+            Console.WriteLine("Second  team\n *****");
+            for (int i = 0; i < _GameMove; i++)
+            {
+                ping2.Hits();
+                pong2.Hits();
+            }
+        }
+    }
+}
+
+
